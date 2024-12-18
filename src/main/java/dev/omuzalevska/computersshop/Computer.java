c1a53ae02ee7525c3a1d82902749b2f9c5f14292
OMuzalevska <oksana.muzalevskaja@gmail.com>
2024-12-18 09:58:24 +0100

    add class Computer

---
+++ src/main/java/dev/omuzalevska/computersshop/Computer.java
@@ -0,0 +1,60 @@
+package dev.omuzalevska.computersshop;
+
+public class Computer {
+    private String brand;
+    private int memory;
+    private String processor;
+    private String operatingSystem;
+    private double price;
+
+    public Computer(String brand, int memory, String processor, String operatingSystem, double price) {
+        this.brand = brand;
+        this.memory = memory;
+        this.processor = processor;
+        this.operatingSystem = operatingSystem;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+
+
+}
